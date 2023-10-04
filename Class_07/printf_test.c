a5b481b80a6a074af7342caf8615356133332607
MarissZZZ <marisszuss@gmail.com>
2023-10-04 10:02:48 +0300

    Class_07b

--- Class_07/printf_test.c
+++ Class_07/printf_test.c
@@ -1,17 +1,25 @@
-/* fflush example */
+/* scanf example */
 #include <stdio.h>
-char mybuffer[80];
-int main()
+
+int main ()
 {
-   FILE * pFile;
-   pFile = fopen ("example.txt","r+");
-   if (pFile == NULL) perror ("Error opening file");
-   else {
-     fputs ("test",pFile);
-     fflush (pFile);    // flushing or repositioning required
-     fgets (mybuffer,80,pFile);
-     puts (mybuffer);
-     fclose (pFile);
-     return 0;
-  }
+  char str [80];
+  int TagGads;
+  int DzimGads;
+  int gadi;
+
+  printf ("Kāds ir tavs vārds: ");
+  scanf ("%79s",str); 
+
+  printf("Kurš gads ir tagad?\n");
+  scanf("%d", &TagGads);
+  printf("Kurā gadā Tu esi dzimis?\n");
+  scanf("%d", &DzimGads);
+
+  gadi = TagGads - DzimGads;
+
+  printf ("%s , %d gadus vecs.\n", str, gadi);
+
+  return 0;
+
 }
