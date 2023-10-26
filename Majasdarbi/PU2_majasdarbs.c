35852fe78c5d1d52a7b8423b805c91296d4d2a9d
MarissZZZ <marisszuss@gmail.com>
2023-10-26 21:04:58 +0300

    Class_md

--- Majasdarbi/PU2_majasdarbs.c
+++ Majasdarbi/PU2_majasdarbs.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    unsigned char skaitlis;
+    int skaitlis;
     char i;
 
-    printf("Ievadiet naturālu decimālu skaitli: ");
-    scanf("%hhu", &skaitlis);
+    do {
+        printf("Ievadiet naturālu decimālu skaitli (1-255): "); //LAI IEVADITAIS SKAITLIS BUTU NATURALS
+        if (scanf("%d", &skaitlis) != 1 || skaitlis <= 0 || skaitlis > 255) {
+            printf("Lūdzu ievadiet naturālu decimālu skaitli no 1 līdz 255.\n");
+            while (getchar() != '\n');
+            skaitlis = -1;  //atgriez lai turpinatos loops
+        }
+    } while (skaitlis <= 0 || skaitlis > 255);
 
-    if (skaitlis < 0) {
-        printf("Lūdzu ievadiet naturālu decimālu skatili (pozitīvu).\n");
-        return 1;
-    }
+    unsigned char skaitlis_unsigned = (unsigned char)skaitlis;
 
     printf("Ievadītais skaitlis bināri: ");
     for (i = 7; i >= 0; i--) {
-        char biti = (skaitlis >> i) & 1;
-        if (skaitlis & biti) printf("1");
-        else printf("0");
+        char biti = (skaitlis_unsigned >> i) & 1;
+        if (biti) {
+            printf("1");
+        } else {
+            printf("0");
+        }
     }
     printf("\n");
 
