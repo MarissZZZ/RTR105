ce5674be1750ad60891055c118caba6c864af0ab
MarissZZZ <marisszuss@gmail.com>
2023-12-20 11:15:59 +0200

    Class_20dec

--- darbi/Lab_4/4ld_integral.c
+++ darbi/Lab_4/4ld_integral.c
@@ -1,6 +1,30 @@
-#include <stdio.h>
+#undef __STRICT_ANSI__
 
-// aproksimēt
-// ar taisnstūriem mēģina aprēķināt laukumu.
-// integral1 = integral2 + 2eps
-// wolfram alpha integrate sin^2(x) from -2pi to 2pi
+#include<stdio.h>
+#include<math.h>
+
+//aproksimēt
+//artaisnstūriemmēģinaaprēķinātlaukumu.
+//integral1=integral2+2eps
+//wolframalphaintegratesin^2(x)from-2pito2pi
+
+void main(){
+
+    float a=0.,b=M_PI/2,eps=1.e-3,h,integr1=0.,integr2;
+    int k,n=2;
+
+    integr2=(b-a)*(sin(a)+sin(b))/n; //(b−a)/2.∗sin(a)+(b−a)/2.∗sin(b);
+    while(fabs(integr2-integr1)>eps)
+    {
+        n*=2;
+        h=(b-a)/n;
+        integr1=integr2;
+        integr2=0.;
+        for(k=0;k<n;k++)
+        {
+            integr2+=h*sin(a+(k+0.5)*h);
+        }
+    printf("Integraalja veertiiba:%.2f\n",integr2);    
+    }
+    printf("Integraalja veertiiba:%.2f\n",integr2);
+}
---
+++ darbi/Lab_5/5ld_statistics.c
@@ -0,0 +1,6 @@
+// iet cauri no lietotaja ievadita teikuma un atrod simbolu ar vismazako ascii kodu un ar lielako izmantokot flag, atstarpes un punktus neņem vērā
+// burtu ar videjo vertibu char - burtam, summai ir int, nanoapaļo un atbilstoši parada simbolu kads sanak
+// jaatrod mediana (sakuma jasakarto atbilstošā secībā)
+// atrod modu. kurs simbols atkartojas, (katrs simbols, cik reizes atkartojas) secibu caurskata vienu reizi
+// burbuļa metode v1 > v2; v1 < v2, maina katrus divus elementus un domā vai apmainīt vai nē no elementu rindas (8 5) 25 --> 5 (8 25)
+// burbuļa metode ir jaimplimentē pašiem, neizmantot gatavas funkcijas
