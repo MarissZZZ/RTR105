8ef0af27cd51d48bceccca2d19f6c4bc221b233a
MarissZZZ <marisszuss@gmail.com>
2024-01-03 23:45:08 +0200

    3jan

--- darbi/Lab_1/1ld_series.c
+++ darbi/Lab_1/1ld_series.c
@@ -37,22 +37,20 @@ int main() {
     yy = lietotaja_sinuss_kvadrats(x);
     printf("y=sinuss(%.2Lf)*sinuss(%.2Lf) ar summu=%.30Lf\n", x, x, yy);
 
-    printf("\n%21s\n", "500");
-    printf("%40s\n", "————————");
-    printf("%17s%11s%7s%8s\n", "\\", "k+1", "2*k", "2*k-1");
-    printf("%6s%12s%20s\n", "2", "\\", "(-1)  *  x  *  2");
-    printf("y=sin(%.2Lf)=%.2Lf", x, y);
-    printf("%2s%2s%50s\n", "=", ">", "————————————————");
-    printf("%18s%15s\n", "/", "(2 * k)!");
-    printf("%17s\n", "/");
-    printf("%40s\n", "————————");
-    printf("\n%21s\n", "k=1");
-    printf("\n");
-    printf("%38s\n", "2");
-    printf("%37s\n", "(-2) * x");
-    printf("rekurences reizinatajs: ");;
-    printf("%50s\n", "————————————————");
-    printf("%42s\n", "k * (2 * k - 1)");
-    printf("\n");
+    printf( "                 500                               \n"
+            "                ------                             \n"
+            "                \\             k+1   2*k   2*k-1   \n"
+            "   2             \\        (-1)  *  x  *  2        \n"
+            "sin(%.2Lf) = %.2Lf = >  -------------------------- \n"
+            "                  /              (2 * k)!          \n"
+            "                 /                                 \n"
+            "                ------                             \n"
+            "                 k = 1                             \n", x, y);
+
+
+    printf( "                               2    2       \n"
+            "                            (-2) * x        \n"
+            "rekurences reizinatajs: ------------------- \n"
+            "                          k * (2 * k - 1)   \n" );
 
 }
--- labIzstrade/1ld_seriesTest.c
+++ labIzstrade/1ld_seriesTest.c
@@ -26,4 +26,22 @@ void main() {
     yy = lietotaja_sinuss_kvadrats(x);
     printf("Lietotaaja funkcija: y=sinuss_kvadrats(%.2f)*sinuss_kvadrats(%.2f)=%.2f\n", x, x, yy);
 
+    printf("\n%21s\n", "500");
+    printf("%40s\n", "————————");
+    printf("%17s%11s%7s%8s\n", "\\", "k+1", "2*k", "2*k-1");
+    printf("%6s%12s%20s\n", "2", "\\", "(-1)  *  x  *  2");
+    printf("y=sin(%.2Lf)=%.2Lf", x, y);
+    printf("%2s%2s%50s\n", "=", ">", "————————————————");
+    printf("%18s%15s\n", "/", "(2 * k)!");
+    printf("%17s\n", "/");
+    printf("%40s\n", "————————");
+    printf("\n%21s\n", "k=1");
+    printf("\n");
+    printf("%38s\n", "2");
+    printf("%37s\n", "(-2) * x");
+    printf("rekurences reizinatajs: ");;
+    printf("%50s\n", "————————————————");
+    printf("%42s\n", "k * (2 * k - 1)");
+    printf("\n");   
+
 }
