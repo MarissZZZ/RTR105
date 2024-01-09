32a8eec28e6f9d7883407a771421b202f10d2a50
MarissZZZ <marisszuss@gmail.com>
2024-01-10 00:02:25 +0200

    10jan

--- darbi/Lab_2/2ld_roots.c
+++ darbi/Lab_2/2ld_roots.c
@@ -1,38 +1,62 @@
-#undef __STRICT_ANSI__
-
 #include <stdio.h>
 #include <math.h>
 
-float modified_sin(float x, float A)
-{
-    return sin(x)-A;
+float sin_kvadrata(float x, float A);
+
+int main() {
+
+    printf("sin(x)^2-A sakņu meklētājs (Dihotomijas metode)\n");
+    printf("---------------------------------------------------\n");
+
+    float a, x, delta_x, b, A, fa, fb;
+    int itera = 1;
+
+    // ievada intervāla sākuma un beigu vērtības
+    printf("Lūdzu ievadiet mazākās robežas 'a' vērtību: ");
+    scanf("%f", &a);
+    printf("Lūdzu ievadiet lielākās robežas 'b' vērtību: ");
+    scanf("%f", &b);
+    printf("Saknes meklēšanas intervāls [%.3f;%.3f]\n", a, b);
+    printf("---------------------------------------------------\n");
+
+    // ievada nobīdes vērtību un precizitātes vērtību
+    printf("Lūdzu ievadiet nobīdes A vērtību (funkcija tiks bīdīta uz leju vai augšu) A = ");
+    scanf("%f", &A);
+    printf("Lūdzu ievadīt precizitātes vērtību (piem. 0.001): ");
+    scanf("%f", &delta_x);
+    printf("---------------------------------------------------\n");
+
+    // Tiek aprēķinātas funkcijas vērtības intervāla galapunktos
+    fa = sin_kvadrata(a, A);
+    fb = sin_kvadrata(b, A);
+
+    // Pārbauda, vai ir iespējams atrast sakni dotajā intervālā
+    if ((A == 0 && fa * fb < 0) || (A != 0 && fa * fb > 0)) {
+        printf("Intervālā [%.3f;%.3f] sin(x)^2 - %.3f nav sakņu vai ir pāra skaits sakņu\n", a, b, A);
+        return 1;
+    }
+
+    // Iteratīvi meklē sakni intervālā
+    x = a;
+    while (x < b) {
+        x = x + delta_x;
+
+        // Pārbauda, vai tuvums saknei ir mazāks par delta_x
+        if ((sin_kvadrata(x, A) < delta_x) && (sin_kvadrata(x, A) > -delta_x)) {
+            printf("Sakne atrodas tuvu pie vērtībām: \n\t x = %.30f\n", x);
+            printf("\t sin(%.4f)^2 - %.5f = %.30f\n", x, A, sin_kvadrata(x, A));
+            printf("Vajadzīgais iterāciju skaits pie precizitātes %.6f ir %d", delta_x, itera);
+            return 0;
+        }
+        itera++;
+    }
+
+    printf("Lūdzu samazināt precizitāti, lai spētu atrast tuvināto saknes vērtību\n"); // Ziņo par nepieciešamību samazināt precizitāti, ja sakne netika atrasta
+    return 1;
 }
 
-int main(){
-    float a=0.01, b=1.5*M_PI, x, delta_x=1.e-3/*0.001*/,funkca,funkcb,funkcx;
-    int k=0;
+// Definē funkciju sin_kvadrata
 
-    funkca = sin(a); funkcb=sin(b);
-
-    if(funkca*funkcb>0){
-        printf("Intervaalaa[%.2f;%.2f] sin(x)funkcijai",a,b);
-        printf("saknju nav (vai taajaa ir paaru saknju skaits)\n");
-        return 1;}
-
-    printf("sin(%7.3f)=%7.3f\t\t\t\t",a,sin(a));
-    printf("sin(%7.3f)=%7.3f\n",b,sin(b));
-
-    while((b-a)>delta_x){
-        k++; //k=k+1;//k+=1;
-        x=(a+b)/2.;
-        if(funkca*sin(x)>0) //pie a=0 −> funkca=0 −> reizinaajums ir preciizi 0 visulaiku -> visulaika straadaa" b=x
-            a=x;
-        else
-            b=x;
-        printf("%2d. iteraacija: sin(%7.3f)=%7.3f\t",k,a,sin(a));
-        printf("sin(%7.3f)=%7.3f\t",x,sin(x));
-        printf("sin(%7.3f)=%7.3f\n",b,sin(b));}
-
-    printf("Sakne atrodas pie x=%.3f,jo sin(x) ir %.30f\n",x,sin(x));
-
-    return 0;}
+float sin_kvadrata(float x, float A) {
+  return sin(x) * sin(x) - A; // Atgriež sin(x)^2 - A vērtību
+}
